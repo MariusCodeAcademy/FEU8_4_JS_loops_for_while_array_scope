c498be62539729cf0306fbb369c60525d3b838d0
Marius CA <mariuskcodeacademy@gmail.com>
2023-06-30 11:02:32 +0300

    html

--- js/for.js
+++ js/for.js
@@ -11,12 +11,30 @@ for (begin; condition; step) {​
 // atspausdinti nuo 0 iki 9 sk
 // debugger;
 for (let i = 0; i < 10; i++) {
-  console.log('sk:', i);
+  // console.log('sk:', i);
 }
 
 // parasyti cikla kuri atspauzdins 'MikePukuotukas sk' sk turetu
 // buti nuo 13 iki 78
 
 for (let i = 13; i <= 78; i++) {
-  console.log(`MikePukuotukas ${i}`);
+  // console.log(`MikePukuotukas ${i}`);
+}
+
+// atspasdinti skaicius nuo 0 iki 50, kas antra skaiciu
+for (let i = 0; i <= 50; i += 2) {
+  // console.log('i ===', i);
+}
+
+for (let i = 0; i <= 50; i++) {
+  if (i % 2 === 0) {
+    // console.log('i ===', i);
+  }
+  // i++;
+}
+
+// atspausinti su for ciklu skaicius nuo 87 iki 16
+for (let i = 87; i >= 16; i--) {
+  console.log('i ===', i);
+  printHtml(`Labas ${i}`);
 }
