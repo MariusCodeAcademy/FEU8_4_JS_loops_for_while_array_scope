872989ef8a95056b978fe57e39cbef0ceff3990e
Marius CA <mariuskcodeacademy@gmail.com>
2023-06-30 10:45:30 +0300

    sadf

--- js/while.js
+++ js/while.js
@@ -7,11 +7,32 @@ while (condition) {
 }
 */
 
-let i = 0;
+let i = 1;
 
-while (i < 10) {
+while (i <= 10) {
   console.log('sk', i);
   i++;
 }
 
 // do while
+i = 1;
+do {
+  console.log(`sukam cikla ${i}`);
+  i++;
+} while (i <= 10);
+
+console.log('i ===', i);
+
+function makeCycle(howMany) {
+  let i = 1;
+
+  while (i <= howMany) {
+    console.log('makeCycle sk', i);
+    i++;
+  }
+}
+makeCycle(5);
+console.log('====================');
+makeCycle(10);
+console.log('====================');
+makeCycle(100);
